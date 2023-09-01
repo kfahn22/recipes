ee10427b33576660ae4451534b128d22026a22ea
MJ McGuiness <134172537+MJMcGuiness@users.noreply.github.com>
2023-09-01 13:47:30 -0400

    Update aboutMJ.js
    Edited bio 9/1/23

--- src/pages/aboutMJ.js
+++ src/pages/aboutMJ.js
@@ -18,14 +18,18 @@ const aboutMJ = () => {
             />
             <h2>About the Author</h2>
             <p>
-              As a professional chemist for over thirty years, it has been my
+              As a professional chemist for over thirty years, it was part of MJ's
               job to explain the needs and desires, the loves and the hates, the
               habits and quirks of inanimate molecules — perverse little motes
-              with their own individual personalities. As I move into
-              retirement, I’m trying my hand at writing about the needs,
-              desires, loves, hates, habits and quirks of people with actual
-              agency, rather than abstract assemblies of anthropomorphized
-              atoms. This is my first attempt...
+              with their own individual personalities. Throughout his career he
+              had the uneviable task of trying to eliven technical reports, and
+              worst of all, patent applications with enough readable proose such
+              that they were not potent soporifics. Having belatedly recognized
+              that he is not some god-like manipulator of the elements, MJ is 
+              trying his hand at writing about the needs, desires, loves, hates, 
+              habits and quirks of people with actual agency, rather than abstract
+              assemblies of anthropomorphized atoms. The Sybyline is his first attempt...
+              
             </p>
             <Link to="/contact" className="btn">
               Contact
